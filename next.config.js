aa5e1ac22416864a9a6bdbab0446d5d091445164
jannes <jannesdrijkoningen@gmail.com>
2023-01-08 17:02:58 +0100

    fix actions #2 (using yarn?)

--- next.config.js
+++ next.config.js
@@ -9,6 +9,10 @@ const withMDX = require('@next/mdx')({
 module.exports = withMDX({
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
   reactStrictMode: true,
+  images: {
+    loader: 'custom',
+    loaderFile: './imageLoader.js',
+  },
   webpack: (config, options) => {
     config.module.rules.push({
       test: /\.ex\..+/i,
